704b157e1d7ef5efba057c79b8d98ada9aef00b1
Paula Andrzejewska <haftpaula@gmail.com>
2023-02-10 09:41:35 +0100

    changed names to english

--- js/data.js
+++ js/data.js
@@ -8,58 +8,58 @@
 }
 
 {
-    let dlugElement = document.querySelector(".js-dlug");
+    let debtElement = document.querySelector(".js-debt");
     let formElement = document.querySelector(".js-form");
-    let rataElement = document.querySelector(".js-rata");
+    let partElement = document.querySelector(".js-part");
 
     const txt = "";
 
     formElement.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let dlug = dlugElement.value;
-        let cheaper = dlugElement.value * 0.03;
+        let debt = debtElement.value;
+        let cheaper = debtElement.value * 0.03;
 
 
         switch (true) {
-            case (dlug <= 1500):
-                rataElement.innerText = txt + `349 zł*`;
+            case (debt <= 1500):
+                partElement.innerText = txt + `349 zł*`;
                 break;
-            case (dlug <= 2500):
-                rataElement.innerText = txt + `479 zł*`;
+            case (debt <= 2500):
+                partElement.innerText = txt + `479 zł*`;
                 break;
-            case (dlug <= 5000):
-                rataElement.innerText = txt + `559 zł*`;
+            case (debt <= 5000):
+                partElement.innerText = txt + `559 zł*`;
                 break;
-            case (dlug <= 7500):
-                rataElement.innerText = txt + `729 zł*`;
+            case (debt <= 7500):
+                partElement.innerText = txt + `729 zł*`;
                 break;
-            case (dlug <= 10000):
-                rataElement.innerText = txt + `859 zł*`;
+            case (debt <= 10000):
+                partElement.innerText = txt + `859 zł*`;
                 break;
-            case (dlug <= 15000):
-                rataElement.innerText = txt + `1129 zł*`;
+            case (debt <= 15000):
+                partElement.innerText = txt + `1129 zł*`;
                 break;
-            case (dlug <= 20000):
-                rataElement.innerText = txt + `1229 zł*`;
+            case (debt <= 20000):
+                partElement.innerText = txt + `1229 zł*`;
                 break;
-            case (dlug <= 35000):
-                rataElement.innerText = txt + `1369 zł*`;
+            case (debt <= 35000):
+                partElement.innerText = txt + `1369 zł*`;
                 break;
-            case (dlug <= 50000):
-                rataElement.innerText = txt + `1499 zł*`;
+            case (debt <= 50000):
+                partElement.innerText = txt + `1499 zł*`;
                 break;
-            case (dlug <= 75000):
-                rataElement.innerText = txt + `1699 zł*`;
+            case (debt <= 75000):
+                partElement.innerText = txt + `1699 zł*`;
                 break;
-            case (dlug <= 100000):
-                rataElement.innerText = txt + `2399 zł*`;
+            case (debt <= 100000):
+                partElement.innerText = txt + `2399 zł*`;
                 break;
-            case (dlug <= 150000):
-                rataElement.innerText = txt + `3249 zł*`;
+            case (debt <= 150000):
+                partElement.innerText = txt + `3249 zł*`;
                 break;
-            case (dlug > 150000):
-                rataElement.innerText = txt + cheaper.toFixed(2);
+            case (debt > 150000):
+                partElement.innerText = txt + cheaper.toFixed(2);
                 break;
         }
     });
